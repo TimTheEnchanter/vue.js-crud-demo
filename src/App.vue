f4812c69b6bfd9adcee6bc087554b0378b5379a9
James Parrott <japarrott@shaw.ca>
2024-11-04 09:59:17 -0700

    add basic input and buttons that will be used for crud with function calls

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,18 @@ const last = ref('')
 </script>
 
 <template>
+  <div><input v-model="prefix" placeholder="Filter prefix"></div>
 
 
 
+  <label>Name: <input v-model="first"></label>
+  <label>Surname: <input v-model="last"></label>
 
-
+  <div class="buttons">
+    <button @click="create">Create</button>
+    <button @click="update">Update</button>
+    <button @click="del">Delete</button>
+  </div>
 </template>
 
 <style>
