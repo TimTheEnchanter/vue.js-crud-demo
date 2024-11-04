f9aa2bea6fcb7e95009c15acada313db076461c6
James Parrott <japarrott@shaw.ca>
2024-11-04 09:51:43 -0700

    instantiate variables

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref, reactive, computed, watch } from 'vue'
+const names = reactive(['Gruber, Hans', 'Bellic, Niko', 'Commando, Captain'])
+const selected = ref('')
+const prefix = ref('')
+const first = ref('')
+const last = ref('')
+
+
+
+
+</script>
+
+<template>
+
+
+
+
+
+</template>
+
+<style>
+* {
+  font-size: inherit;
+}
+
+
+
+
+
+
+
+</style>
